dfb5414f8d6af9ff4e464bd94cfda55740f44cef
Seth Davenport <seth@rangle.io>
2016-01-04 16:20:15 -0500

    [Fixes #3] Fixed some lint, tsc warnings.
    Also fixed two webpack warnings about pre-bundled code.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="redux-logger/redux-logger.d.ts" />
 /// <reference path="redux/redux.d.ts" />
 /// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts" />
+/// <reference path="assertion-error/assertion-error.d.ts" />
+/// <reference path="chai/chai.d.ts" />
