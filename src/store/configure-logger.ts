f5b9cad03bc764866f08bb4955e3e73b6643b376
Evan Schultz <evan@rangle.io>
2016-01-12 15:50:57 -0500

    Only import Iterable from Immutable
    * Change configure-logger.ts to only import Iterable from
      immutable instead of everything

--- src/store/configure-logger.ts
+++ src/store/configure-logger.ts
@@ -1,6 +1,7 @@
 declare let __DEV__: any;
-import * as Immutable from 'immutable';
+import {Iterable} from 'immutable';
 const createLogger = require('redux-logger');
+
 const logger = createLogger({
     level: 'info',
     collapsed: true,
@@ -8,7 +9,7 @@ const logger = createLogger({
     stateTransformer: (state) => {
         let newState = {};
         for (var i of Object.keys(state)) {
-            if (Immutable.Iterable.isIterable(state[i])) {
+            if (Iterable.isIterable(state[i])) {
                 newState[i] = state[i].toJS();
             } else {
                 newState[i] = state[i];
