6212996e17f4008000d105f291552b3bc4ffea89
Eric Jimenez <eric.jimenez@rangle.io>
2016-01-25 15:07:00 -0500

    chore: remove extra space

--- src/components/counter/counter.ts
+++ src/components/counter/counter.ts
@@ -4,7 +4,7 @@ import {Component, View} from 'angular2/core';
   selector: 'counter',
   inputs: [
     'counter',
-    'increment', 
+    'increment',
     'decrement',
     'incrementIfOdd',
     'incrementAsync'
