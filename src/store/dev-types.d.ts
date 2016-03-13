87d591c7a31b4d9d30d6b95db0d92a0a70a0f7fe
Seth Davenport <seth@rangle.io>
2016-03-13 18:30:19 -0400

    Enabled the Chrome extension version of redux dev tools.
    Got rid of references to the old one.
    
    Cleaned up configure-store a bit.
    
    Connected to rangle/rangle-starter#39

---
+++ src/store/dev-types.d.ts
@@ -0,0 +1,9 @@
+// Types for various development options.
+
+// A global boolean that turns on or off devmode in the client.
+declare let __DEV__: boolean;
+
+// A hack for the Redux DevTools Chrome extension.
+interface Window {
+  devToolsExtension?: () => void;
+}
